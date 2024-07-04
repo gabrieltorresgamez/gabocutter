9fc7ed3062baed914462f1cc79386da5aa23ac75
Gabriel Torres Gamez <gabriel.torres@fuet.ch>
2024-07-04 19:28:41 +0200

    add clean docstrings

--- src/gabocutter/utils/misc.py
+++ src/gabocutter/utils/misc.py
@@ -1,3 +1,16 @@
-def get_hello_world():
-    """Return a simple hello world message."""
+"""
+Miscellaneous utility functions.
+
+These functions are not specific to any particular part of the codebase, and are
+used in multiple places throughout the project.
+"""
+
+
+def get_hello_world() -> str:
+    """
+    Return a simple hello world message.
+
+    Returns:
+        str: A string containing the hello world message.
+    """
     return "Hello, World!"
